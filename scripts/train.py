5958efebfb08ca5fdd0c117dcd0142dc30ac16f8
Yeshwanth I <yeshwanth@Yeshwanth-mac.local>
2026-01-09 23:38:37 +0530

    rand forest n_est=50,max-dep=8

--- scripts/train.py
+++ scripts/train.py
@@ -21,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Train model
-model = LinearRegression()
+model = RandomForestRegressor(random_state=42,n_estimators=50,max_depth=8)
 model.fit(X_train, y_train)
 
 # Predict
