4facc7d4c9e93e396a1a291ef91dc7bfbc55dcee
Yeshwanth I <yeshwanth@Yeshwanth-mac.local>
2026-01-09 23:20:03 +0530

    Linear Reg

--- scripts/train.py
+++ scripts/train.py
@@ -4,6 +4,7 @@ import json
 import math
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 
@@ -19,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Train model
-model = RandomForestRegressor(random_state=42,n_estimators=100,max_depth=8)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Predict
