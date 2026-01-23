c6acb408dfb61d04349e16d945c9048ab9d97932
Yeshwanth I <yeshwanth@Yeshwanth-mac.local>
2026-01-23 15:49:33 +0530

    model deployment

--- scripts/train.py
+++ scripts/train.py
@@ -1,13 +1,11 @@
 import pandas as pd
-import numpy as np
 import json
 import math
+import joblib
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-
 from sklearn.metrics import mean_squared_error, r2_score
-import joblib
 
 # Load dataset
 data = pd.read_csv("data/wine+quality/winequality-red.csv", sep=";")
@@ -15,13 +13,17 @@ data = pd.read_csv("data/wine+quality/winequality-red.csv", sep=";")
 X = data.drop("quality", axis=1)
 y = data["quality"]
 
-# Train-test split
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
 # Train model
-model = RandomForestRegressor(random_state=42,n_estimators=50,max_depth=8)
+model = RandomForestRegressor(
+    random_state=42,
+    n_estimators=50,
+    max_depth=8
+)
 model.fit(X_train, y_train)
 
 # Predict
@@ -31,17 +33,17 @@ y_pred = model.predict(X_test)
 rmse = math.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
 
-# Print metrics (for logs)
 print(f"RMSE: {rmse}")
 print(f"R2 Score: {r2}")
 
-# Save results
-results = {
+# Save model
+joblib.dump(model, "model.pkl")
+
+# Save metrics
+metrics = {
     "rmse": rmse,
     "r2": r2
 }
 
-joblib.dump(model, "model.pkl")
-
-with open("results.json", "w") as f:
-    json.dump(results, f, indent=4)
+with open("metrics.json", "w") as f:
+    json.dump(metrics, f, indent=4)
